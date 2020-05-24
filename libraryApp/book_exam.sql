d5443135225d4a741e8a7ba2acf65ff4819162e1
leeshinyook <tlsdbr97@gmail.com>
2020-05-25 01:12:42 +0900

    Add: book db sample

---
+++ libraryApp/book_exam.sql
@@ -0,0 +1,11 @@
+insert into book(title) values('Typescript quick start');
+insert into book(title) values('The selfish gene');
+insert into book(title) values('Springboot quick start');
+insert into book(title) values('Operating system');
+insert into book(title) values('God of java');
+insert into book(title) values('Spring quick start');
+insert into book(title) values('Algorithm training');
+insert into book(title) values('Effective java');
+insert into book(title) values('DL learning from Scratch');
+insert into book(title) values('Object');
+insert into book(title) values('Inside Javascript');
