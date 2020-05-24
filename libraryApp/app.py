cac4838e3619c6b29e1f781abea936a7c1eb420d
leeshinyook <tlsdbr97@gmail.com>
2020-05-25 01:40:11 +0900

    Add: API 기본적인 틀 생성

--- libraryApp/app.py
+++ libraryApp/app.py
@@ -1,12 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+  return render_template('index.html')
+
+@app.route('/searchBook/<book>')
+def searchBook(book):
+  print(book)
+  return jsonify({"book" : book})
+
+# 
+@app.route('/rentBook', methods = ['PUT'])
+def rentBook():
+  book = request.get_json()
+  return
+
+@app.route('/returnBook', methods = ['PUT'])
+def returnBook():
+  return
 
 
-@app.route('/info')
-def info():
-    return 'Info'
+@app.route('/searchAllBook')
+def searchAllBook():
+  return 
+
+if __name__ == "__main__":
+  app.run()
